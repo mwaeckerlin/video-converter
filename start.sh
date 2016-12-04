57faab74a2609d84d66edb9bd08cf0fa40528bd0
Marc Wäckerlin <marc@waeckerlin.org>
2016-12-04 20:08:10 +0100

    improved version

--- start.sh
+++ start.sh
@@ -1,37 +1,46 @@
 #!/bin/bash -e
 
-convert() {
+target() {
     f="$1"
-    for format in ${FORMATS}; do
-        flags=FLAGS_$format
-        t="${f/$SRC/$DST}"
-        t="${t%.*}.${format}"
-        if test -e "$t"; then
-	    rm "$t"; # remove existing file
-        fi
-        test -d "${t%/*}" || mkdir -p "${t%/*}"
-        file "$f"
-        echo "++++ $f → $t"
-        if ( ! avconv -i "$f" ${!flags} -filter:v yadif "$t" 2> /dev/null > /dev/null \
-                   || ! test -s "${t}") && test -e "$t"; then
-            echo "**** ERROR: avconv -i '$f' ${!flags} -filter:v yadif '$t'"
-	    rm "$t"; # remove file if conversion failed
-        fi
-    done
+    format="$2"
+    t="${f/$SRC/$DST/$format}"
+    t="${t%.*}.${format}"
+    echo -n "$t"
 }
 
-echo "**** converting existing files"
-for f in $(find $SRC -name '.*' -prune -o -type f -print); do
-    t="${f/$SRC/$DST}"
-    t="${t%.*}.${format}"
-    if test -s "${t}"; then
-	continue; # ignore if file exists and is not empty
+convert() {
+    f="$1"
+    format="$2"
+    flags=FLAGS_$format
+    t=$(target "$f" "$format")
+    if test -e "$t"; then
+	rm "$t"; # remove existing file
     fi
-    convert "$f"
+    test -d "${t%/*}" || mkdir -p "${t%/*}"
+    echo "++++ $f → $t"
+    if ( ! avconv -i "$f" ${!flags} -filter:v yadif "$t" 2> /dev/null > /dev/null \
+               || ! test -s "${t}") && test -e "$t"; then
+        file "$f"
+        echo "**** ERROR: avconv -i '$f' ${!flags} -filter:v yadif '$t'"
+	rm "$t"; # remove file if conversion failed
+    fi
+}
+
+echo "==== converting existing files"
+for f in $(find $SRC -name '.*' -prune -o -type f -print); do
+    for format in ${FORMATS}; do
+        t=$(target "$f" "$format")
+        if test -s "${t}"; then
+	    continue; # ignore if file exists and is not empty
+        fi
+        convert "$f" "$format"
+    done
 done
 
 echo "==== initialized, starting service"
 inotifywait -r --format '%w' -e modify,attrib,move,create,delete ${SRC} |
     while read p; do
-        convert "$p"
+        for format in ${FORMATS}; do
+            convert "$p" "$format"
+        done
     done
