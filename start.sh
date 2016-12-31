41ee2034952473617cd52018a12a80efa3e71a65
Marc Wäckerlin <marc@waeckerlin.org>
2016-12-31 13:50:08 +0100

    add date time to logs

--- start.sh
+++ start.sh
@@ -17,16 +17,16 @@ convert() {
 	rm "$t"; # remove existing file
     fi
     test -d "${t%/*}" || mkdir -p "${t%/*}"
-    echo "++++ $f → $t"
+    echo "++++ $(date) $f → $t"
     if ( ! avconv -i "$f" ${!flags} -filter:v yadif "$t" 2> /dev/null > /dev/null \
                || ! test -s "${t}") && test -e "$t"; then
         file "$f"
-        echo "**** ERROR: avconv -i '$f' ${!flags} -filter:v yadif '$t'"
+        echo "**** $(date) ERROR: avconv -i '$f' ${!flags} -filter:v yadif '$t'"
 	rm "$t"; # remove file if conversion failed
     fi
 }
 
-echo "==== converting existing files"
+echo "==== $(date) converting existing files"
 for f in $(find $SRC -name '.*' -prune -o -type f -print); do
     for format in ${FORMATS}; do
         t=$(target "$f" "$format")
@@ -37,7 +37,7 @@ for f in $(find $SRC -name '.*' -prune -o -type f -print); do
     done
 done
 
-echo "==== initialized, starting service"
+echo "==== $(date) initialized, starting service"
 inotifywait -r --format '%w' -e modify,attrib,move,create,delete ${SRC} |
     while read p; do
         if test -f "-p"; then
@@ -48,7 +48,7 @@ inotifywait -r --format '%w' -e modify,attrib,move,create,delete ${SRC} |
             for format in ${FORMATS}; do
                 t=$(target "$f" "$format")
                 if test -f "$t"; then
-                    echo "---- $t"
+                    echo "---- $(date) $t"
                     rm "$t"
                 fi
             done
